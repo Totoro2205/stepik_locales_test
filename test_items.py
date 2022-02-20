3082cc87a8d7f8e737ee84ae903fa14b8677fcab
Totoro2205 <alexander.keepsolid@gmail.com>
2022-02-20 16:21:10 +0200

    url fix

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_210/'
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_cart_button_should_be_present_on_product_page(browser):
     browser.get(url)
