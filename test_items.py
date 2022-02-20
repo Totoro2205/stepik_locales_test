25c70c971262d222b0a2aeb895b5db565fb0cdbb
Totoro2205 <alexander.keepsolid@gmail.com>
2022-02-20 16:19:26 +0200

    some fixes

--- test_items.py
+++ test_items.py
@@ -3,12 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_210/'
 
 def test_cart_button_should_be_present_on_product_page(browser):
     browser.get(url)
     #time.sleep(30)
-    cartbutton = WebDriverWait(browser, 10).until(
-                 EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn.btn-add-to-basket"))
-                 )
-    assert cartbutton, "'Add to cart' button is broken"
+    cartbutton = None
+    try:
+        cartbutton = WebDriverWait(browser, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn.btn-add-to-basket"))
+                    )
+    except Exception as ex:
+        assert cartbutton, "'Add to cart' button is broken"
+    assert True
